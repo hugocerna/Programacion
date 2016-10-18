09ffa606ee0a83d8788b323ee45e9fd1660e9994
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2016-10-18 17:16:13 -0500

    Update ascii.c

--- Cadenas-de-caracteres/ascii.c
+++ Cadenas-de-caracteres/ascii.c
@@ -1,15 +1,17 @@
+// Programa que muestra el código ASCII de algunos caracteres 
 #include<stdio.h>
 
 void main ()
 {
-	unsigned int i = 33;
+	int ch, i = 33;
 	for ( ; i < 127 ; i++ )
 	{
 		printf("%c%cAl codigo ASCII %d le corresponde el caracter %c%c%c",62,32,i,9,i,10);
-		if (i%32 == 0) 
+		if (i%20 == 0) 
 		{
-			printf("Presione enter para continuar...");
-			getchar();
+			printf(" Presione enter para continuar...");
+			ch = getchar(); // getchar() retorna 10
+			printf(" ch = %d, ch = %c", ch, ch);
 		}
 	}
 }
