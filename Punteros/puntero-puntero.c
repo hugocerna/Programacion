5b5172a2cbba8dcddfdb312bdace1c7b8deb5544
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2016-10-25 16:56:26 -0500

    Create puntero-puntero.c

---
+++ Punteros/puntero-puntero.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Declarando e inicializando una variable entera */
+int joaquin = 5;
+/* Declarando un puntero a entero */
+int * johel;
+/* Declarando un puntero a puntero a entero */
+int ** rodrigo;
+
+void main ()
+{
+	johel = &joaquin;
+    rodrigo = &johel;    
+	
+	/* Accesando a var directamente e indirectamente */
+	printf("\n Acceso directo: joaquin = %d", joaquin);
+	printf("\n Acceso indirecto por johel: joaquin = %d", *johel);	
+	printf("\n Acceso indirecto desde rodrigo: joaquin = %d\n\n", **rodrigo);
+	
+	printf(" &joaquin = %p\n", &joaquin);
+	printf(" johel = %p\n\n", johel);
+	
+	printf(" &johel = %p\n", &johel);
+	printf(" rodrigo = %p\n\n", rodrigo);
+		
+}
